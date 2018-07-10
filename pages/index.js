ed974d02d5495ee7ef1040b9690d4160d3a3e4ad
jsDotCr <rocco@js.cr>
2018-07-10 09:28:41 +0200

    feat: add basic bootstrap-based layout

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-export default function indexPage () {
-  return (
+import Layout from '../src/components/layout/Layout'
+
+export default () => (
+  <Layout>
     <main>
       <h1>Open Budget WebClient</h1>
     </main>
-  )
-}
+  </Layout>
+)
---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default ({ children, title = 'Open Budget Client' }) => (
+  <React.Fragment>
+    <Head>
+      <title>{ title }</title>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1, width=device-width, shrink-to-fit=no' />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    </Head>
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link href="#">
+          <a className="navbar-brand">Home</a>
+        </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <Link href='/organization'><a className="nav-link">Organizations</a></Link>
+            <Link href='/card'><a className="nav-link">Cards</a></Link>
+            <Link href='/transaction'><a className="nav-link">Transactions</a></Link>
+            <Link href='/transaction-type'><a className="nav-link">Transaction types</a></Link>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <main className="container-fluid">
+      { children }
+    </main>
+    <footer className="container-fluid">
+      {'I`m here to stay'}
+    </footer>
+  </React.Fragment>
+)
