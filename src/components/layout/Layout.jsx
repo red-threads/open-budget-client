0e153e3d985afdacce5b5febda2cec9ffc45c112
jsDotCr <rocco@js.cr>
2018-07-23 00:09:29 +0200

    fix: crossorigin -> crossOrigin, and footer copy

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -8,10 +8,10 @@ export default ({ children, title = 'Open Budget Client' }) => (
       <title>{ title }</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1, width=device-width, shrink-to-fit=no' />
-      <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' integrity='sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB' crossorigin='anonymous' />
-      <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous' />
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymous' />
-      <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js' integrity='sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T' crossorigin='anonymous' />
+      <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' integrity='sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB' crossOrigin='anonymous' />
+      <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossOrigin='anonymous' />
+      <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossOrigin='anonymous' />
+      <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js' integrity='sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T' crossOrigin='anonymous' />
     </Head>
     <header>
       <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
@@ -31,11 +31,11 @@ export default ({ children, title = 'Open Budget Client' }) => (
         </div>
       </nav>
     </header>
-    <main className='container-fluid'>
+    <main className='container py-4'>
       { children }
     </main>
-    <footer className='container-fluid'>
-      {'I`m here to stay'}
+    <footer className='container-fluid py-4 text-center text-small bg-light text-danger'>
+      RedThreads
     </footer>
   </React.Fragment>
 )
