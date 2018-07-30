2beb0b5f088389e1c6c72a1c4d31fe0d79d17de4
jsDotCr <rocco@js.cr>
2018-07-30 11:42:35 +0200

    feat: add custom error page

---
+++ pages/_error.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default class Error extends React.Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    console.log(err)
+    return { err, statusCode }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <p>
+          {this.props.statusCode
+            ? `An error ${this.props.statusCode} occurred on server`
+            : 'An error occurred on client'}
+        </p>
+        {
+          process.env.NODE_ENV !== 'production' && (
+            <pre>{JSON.stringify(this.props.err, '\t', 2)}</pre>
+          )
+        }
+      </React.Fragment>
+    )
+  }
+}
