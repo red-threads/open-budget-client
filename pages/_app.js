f4249e181028dc6048af3f0d4a68bc9ba16aeb80
jsDotCr <rocco@js.cr>
2018-08-06 09:33:34 +0200

    feat: implement notifications

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import App, { Container } from 'next/app'
 import Head from 'next/head'
 import React from 'react'
+import Alert from 'react-s-alert'
 
 import rollbar from '../src/rollbar'
 rollbar()
@@ -15,6 +16,7 @@ export default class OpenBudgetApp extends App {
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         </Head>
         <Component {...pageProps} />
+        <Alert stack={{limit: 3}} effect="stackslide" position="bottom" preserveContext />
       </Container>
     )
   }
--- pages/edit.js
+++ pages/edit.js
@@ -1,6 +1,9 @@
 import Debug from 'debug'
+import camelCase from 'lodash.camelcase'
+import Link from 'next/link'
 import React from 'react'
 import Form from 'react-jsonschema-form'
+import Alert from 'react-s-alert'
 
 import { default as jsonApi } from '../src/api'
 import { default as ExtRefSelect } from '../src/components/external-references/Select'
@@ -17,9 +20,10 @@ export default class extends React.Component {
   static async getInitialProps ({ query: { id, entity } }) {
     debug('gip', id)
     debug('entity', entity)
-    setModels[entity]()
-    const schema = await toForm(schemas[entity])
-    const include = defaultIncludes[entity].join(',')
+    const camelCaseEntity = camelCase(entity)
+    setModels[camelCaseEntity]()
+    const schema = await toForm(schemas[camelCaseEntity])
+    const include = defaultIncludes[camelCaseEntity].join(',')
     const apiOptions = Object.assign({},
       include ? { include } : {}
     )
@@ -29,10 +33,11 @@ export default class extends React.Component {
     return {
       action: id ? 'edit' : 'create',
       apiOptions,
+      camelCaseEntity,
       entity,
       id,
       initialData: id
-        ? await jsonApi.find(entity, id, apiOptions)
+        ? await jsonApi.find(camelCaseEntity, id, apiOptions)
         : defaultValues,
       schema
     }
@@ -49,6 +54,7 @@ export default class extends React.Component {
         abortEarly: false
       })
     } catch ({ inner: validationErrors }) {
+      Alert.error(`Validation errored! ${validationErrors.length} errors found`)
       validationErrors.forEach(error => {
         const basePath = error.path.split('.')[0]
         errors[basePath].addError(error.message)
@@ -64,8 +70,22 @@ export default class extends React.Component {
 
   async onSubmit ({ formData }) {
     const { apiOptions, entity, action } = this.props
+    if (action === 'create') {
+      delete formData.id
+    }
     const response = await jsonApi[action === 'edit' ? 'update' : 'create'](entity, formData, apiOptions)
     console.log(response)
+    if (response.errors) {
+      Alert.error(`Entity ${action} failed! ${JSON.stringify(response.errors)}`)
+    } else {
+      Alert.success(
+        <div>Entity {action} succeeded! Link: 
+          <Link href={`/${entity}/${response.data.id}`}>
+            <a>{response.data.id}</a>
+          </Link>
+        </div>
+      )
+    }
   }
 
   render () {
