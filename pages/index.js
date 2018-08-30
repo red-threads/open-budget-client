63b68fe29bd3cb337f80619bef77a25e5aa8a7f2
jsDotCr <rocco@js.cr>
2018-08-31 00:16:24 +0200

    feat: add auth0 logo

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,14 @@ export default () => (
   <Layout>
     <main>
       <h1>Open Budget WebClient</h1>
-      <Link href='/login'>
-        <a>Please login</a>
-      </Link>
+      <p>
+        <Link href='/login'>
+          <a>Please login</a>
+        </Link>
+      </p>
+      <p>
+        Authentication provided by <a width="150" height="50" href="https://auth0.com/?utm_source=oss&utm_medium=gp&utm_campaign=oss" target="_blank" alt="Single Sign On & Token Based Authentication - Auth0"><img width="150" height="50" alt="JWT Auth for open source projects" src="//cdn.auth0.com/oss/badges/a0-badge-light.png"/></a>
+      </p>
     </main>
   </Layout>
 )
