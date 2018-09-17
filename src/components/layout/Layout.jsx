dd734ee31b4a94d7af4c99051ca6f19ee0a00f9f
jsDotCr <rocco@js.cr>
2018-09-17 21:51:33 +0200

    feat: offline-friendly vendors

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -10,12 +10,12 @@ export function Layout ({ children, title = 'Open Budget Client', onDemandRoleCh
         <title>{ title }</title>
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1, width=device-width, shrink-to-fit=no' />
-        <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' integrity='sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB' crossOrigin='anonymous' />
-      <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossOrigin='anonymous' />
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossOrigin='anonymous' />
-      <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js' integrity='sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T' crossOrigin='anonymous' />
-      <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/react-s-alert@1.4.1/dist/s-alert-default.css' crossOrigin='anonymous' />
-      <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/react-s-alert@1.4.1/dist/s-alert-css-effects/stackslide.css' crossOrigin='anonymous' />
+        <link rel='stylesheet' href='/static/bootstrap/4.1.3.css' crossOrigin='anonymous' />
+        <script src='/static/jquery/3.3.1.slim.js' crossOrigin='anonymous' />
+        <script src='/static/popper.js/1.14.4.js' crossOrigin='anonymous' />
+        <script src='/static/bootstrap/4.1.3.js' crossOrigin='anonymous' />
+        <link rel='stylesheet' href='/static/react-s-alert/1.4.1.default.css' crossOrigin='anonymous' />
+        <link rel='stylesheet' href='/static/react-s-alert/1.4.1.stackslide.css' crossOrigin='anonymous' />
       </Head>
       <header>
         <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
