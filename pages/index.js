34a68cae5f2e954ecd1ea046a9c906159925bbe0
jsDotCr <rocco@js.cr>
2018-09-23 13:19:46 +0200

    fix: index page styles

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,13 @@ import Layout from '../src/components/layout/Layout'
 
 export default () => (
   <Layout>
-    <main>
-      <h1>Open Budget WebClient</h1>
-      <p>
-        <Link href='/login'>
-          <a>Please login</a>
-        </Link>
-      </p>
+    <main className="jumbotron">
+      <h1 className="display-4">Open Budget</h1>
+      <p className="lead">The web client</p>
+      <Link href='/login'>
+        <a className="btn btn-outline-dark btn-lg" role="button">Please login</a>
+      </Link>
+      <hr class="my-4" />
       <p>
         Authentication provided by <a width='150' height='50' href='https://auth0.com/?utm_source=oss&utm_medium=gp&utm_campaign=oss' target='_blank' alt='Single Sign On & Token Based Authentication - Auth0'><img width='150' height='50' alt='JWT Auth for open source projects' src='//cdn.auth0.com/oss/badges/a0-badge-light.png' /></a>
       </p>
