2d043b007367ffd3e07fc1cdd2f0b466d94a8c3d
jsDotCr <rocco@js.cr>
2018-10-19 12:18:12 +0200

    feat(upload-batch): show batch list

--- src/components/upload-batch/List.jsx
+++ src/components/upload-batch/List.jsx
@@ -1,26 +1,73 @@
 import Debug from 'debug'
+import Link from 'next/link'
+import * as React from 'react'
+import { addLast } from 'timm'
 import batchTypes from '../../models/batchTypes.map'
 
 const debug = Debug('ob:c:comp:updload-batch-list')
-export default function UploadBatchList (props) {
-  debug(props)
-  if (props.items.length === 0) {
-    return null
+export default class UploadBatchList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: []
+    }
   }
-  const mapper = batchTypes[props.type].mapTo[props.entity]
-  debug(mapper)
-  if (typeof mapper !== 'function') {
-    return null
+  async componentDidMount() {
+    const { type, entity, items } = this.props
+    if (!type || !entity || !items) {
+      debug('CDM: missing type, entity, items', type, entity, items)
+      return
+    }
+    debug('cdm')
+    const mapper = batchTypes[type].mapTo[entity]
+    debug('mapper', mapper)
+    for (let item of items) {
+      const itemsToAdd = await mapper(item)
+      this.setState({
+        items: addLast(this.state.items, itemsToAdd)
+      })
+    }
   }
-  return (
-    <ul>
-      {
-        props.items.map(item => {
-          return (
-            <div>{JSON.stringify(mapper({ entity: props.entity, item }))}</div>
-          )
-        })
-      }
-    </ul>
-  )
+  render() {
+    const { items } = this.state
+    debug('render items', items)
+    if (items.length === 0) {
+      return null
+    }
+    return (
+      <table className='my-4 table table-striped'>
+        <thead className='table-active'>
+          <tr>
+            <th scope="col">Type</th>
+            <th scope="col">API status</th>
+            <th scope="col">Name</th>
+            <th scope="col">Depends on</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            items.map(item => (
+              <tr id={item.localUuid}>
+                <td>{item.entityName}</td>
+                <td>{item.isRemote ? 'On API' : 'Needs save'}{item.didUpdateRemote ? ', updated' : ''}</td>
+                <td>
+                  <Link href={{
+                    pathname: item.isRemote ? `/${item.entityName}/${item.id}` : `/${item.entityName}/new`,
+                    query: !item.isRemote ? item : {}
+                  }}>
+                    <a><pre style={{ whiteSpace: 'pre-wrap' }}>{JSON.stringify(item.resource, '\t', 2)}</pre></a>
+                  </Link>
+                </td>
+                <td>{item.dependsOn.map((dependency = {}) => (
+                  <Link href={`#${dependency.id}`}>
+                    <a>{dependecy.name}</a>
+                  </Link>
+                ))}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    )
+  }
 }
