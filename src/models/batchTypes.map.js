b9424fa7d4dc4a660d38c268db5adad0093e1168
jsDotCr <rocco@js.cr>
2018-10-28 10:02:50 +0100

    feat(upload-batch): first rough implementation
    Transaction-specific implementation of the batch upload

--- src/models/batchTypes.map.js
+++ src/models/batchTypes.map.js
@@ -1,26 +1,89 @@
 import Debug from 'debug'
-import { set } from 'timm'
+import { merge, set, addLast } from 'timm'
 import batchTypes from './batchTypes.json'
-import { findOneOrAll } from '../api'
+import { findOneOrAll, update } from '../api'
 
 const debug = Debug('ob:c:models:batchTypes:map')
+function getLocalUuid() {
+  const id = btoa(Math.floor(Math.random()*1000))
+  return `${new Date().getTime()}-${id}`
+}
 const mappers = {
   csv: {
-    transaction: async function ({ entity, item }) {
-      const gross = Number(item['Amount (EUR)']) * 100
-      debug(entity, item)
-      debug('gross', gross)
-      const isIncoming = gross > 0
-      const payee = await findOneOrAll({
-        entity: 'card',
+    card: async function (item) {
+      const entityName = 'card'
+      debug(entityName)
+      debug(item)
+      const iban = item['Account number']
+      const name = item.Payee
+      const reference = item['Payment reference']
+      debug(iban, name, reference)
+      let isRemote = false
+      let didUpdateRemote = false
+      let hasMultipleChoices = false
+      let resource
+      // Retrieve remote, if any
+      let { data: resources } = await findOneOrAll({
+        entity: entityName,
         options: {
-          filter: `(:or,(iban,:eq,\`${item['Account number']}\`),(name,:eq,\`${item.Payee}\`))`
+          filter: `(:or,(iban,\`${iban}\`),(name,\`${name}\`),(aliases,\`${reference}\`))`
         }
       })
-      return Object.assign({
+      debug('remote call to card', resources)
+      if (resources.length === 1) {
+        isRemote = true
+        resource = resources[0]
+        debug('found a remote card', name)
+        if (resource.iban !== iban || resource.name !== name || !resource.aliases.includes(reference)) {
+          resource = merge(resource, {
+            iban,
+            name,
+            aliases: addLast(resource.aliases, reference)
+          })
+          await update({ entityName, resource })
+          debug('remote card updated', resource)
+          didUpdateRemote = true
+        }
+      } else if (resources.length > 1) {
+        debug('MULTIPLE CHOICES FOUND!')
+        isRemote = true
+        hasMultipleChoices = true
+      } else {
+        debug('card not found remotely, returning its skeleton so it can be created')
+        resource = {
+          name,
+          iban,
+          aliases: [
+            reference
+          ]
+        }
+      }
+      return [
+        {
+          dependsOn: [],
+          didUpdateRemote,
+          entityName,
+          hasMultipleChoices,
+          isRemote,
+          localUuid: getLocalUuid(),
+          resource,
+          resources
+        }
+      ]
+    },
+    transaction: async function (item) {
+      const entityName = 'transaction'
+      debug(entityName)
+      debug(item)
+      const gross = Number(item['Amount (EUR)']) * 100
+      const isIncoming = gross > 0
+      debug('gross', gross, isIncoming)
+      // Retrieve relationships
+      const [ card ] = await mappers.csv.card(item)
+      // Create skeleton
+      const resource = Object.assign({
         createdAt: item.Date,
-        gross,
-        'Payee': '',
+        gross: Math.abs(gross),
         'Account number': '',
         'Transaction type': '',
         'Payment reference': '',
@@ -30,8 +93,20 @@ const mappers = {
         'Type Foreign Currency': '',
         'Exchange Rate': ''
       }, {
-        [isIncoming ? 'from' : 'to']: payee
+        [isIncoming ? 'from' : 'to']: card.resource
       })
+      return [
+        card,
+        {
+          dependsOn: [{ id: card.localUuid, name: card.resource.name }],
+          didUpdateRemote: false,
+          entityName,
+          hasMultipleChoices: false,
+          isRemote: false,
+          localUuid: getLocalUuid(),
+          resource
+        }
+      ]
     }
   }
 }
